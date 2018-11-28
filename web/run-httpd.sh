73f70641b9b9759a6cc64009db95820c6002c6a6
Bohdan Bashynskyi <generydey@localhost.localdomain>
2018-11-28 14:55:16 +0100

    Removed unneded command

--- web/run-httpd.sh
+++ web/run-httpd.sh
@@ -2,7 +2,6 @@
 
 svn export https://github.com/bbashinskiy/jws/trunk/web/jws /var/www/html --force &&\
 
-chmod 777 /var/www/html/cache && \
 chown -Rf apache:apache /var/www/html/*
 
 exec /usr/sbin/apachectl -DFOREGROUND
